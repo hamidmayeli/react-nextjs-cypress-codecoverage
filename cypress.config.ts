1280a9d04754c97b58b79948c21e3388f0235736
Hamid <hamid.mayeli@justeattakeaway.com>
2023-06-23 14:20:23 +0100

    Add CC

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,22 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-
   component: {
     devServer: {
       framework: "next",
       bundler: "webpack",
+    },
+    setupNodeEvents: (on, config) => {
+      require('@cypress/code-coverage/task')(on, config)
+      return config
+    },
+  },
+
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    setupNodeEvents: (on, config) => {
+      require('@cypress/code-coverage/task')(on, config)
+      return config
     },
   },
 });
